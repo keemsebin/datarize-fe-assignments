1a481e6df7229fdfefcfdbbaa38ae33df1b1d3eb
keemsebeen <sebin0580@gmail.com>
2025-12-29 22:12:13 +0900

    feat: PurchaseFrequencyChart 컴포넌트 구현

---
+++ apps/frontend/src/features/Shopping/components/purchase/PurchaseFrequencyChart.tsx
@@ -0,0 +1,24 @@
+import { shoppingQueryOptions } from '@/apis/shopping.queries';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { Flex } from '@/shared/components/Flex/Flex';
+import { DateRangePicker } from './DateRangePicker';
+import { useDateRange } from '../../hooks/useDateRange';
+import { PurchaseBarChart } from './PurchaseBarChart';
+
+export const PurchaseFrequencyChart = () => {
+  const { dateRange, handleDateChange } = useDateRange();
+
+  const { data: purchaseFrequencyData } = useSuspenseQuery(
+    shoppingQueryOptions.purchaseFrequency({
+      from: dateRange.from,
+      to: dateRange.to,
+    }),
+  );
+
+  return (
+    <Flex dir="col" gap={6} className="w-full">
+      <DateRangePicker dateRange={dateRange} onDateChange={handleDateChange} />
+      <PurchaseBarChart data={purchaseFrequencyData} />
+    </Flex>
+  );
+};
