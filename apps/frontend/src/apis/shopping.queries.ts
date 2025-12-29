1cac6f54a65e8c318fb3459516060fb56285eb7e
keemsebeen <sebin0580@gmail.com>
2025-12-30 01:03:04 +0900

    feat: customers type, api 추가

--- apps/frontend/src/apis/shopping.queries.ts
+++ apps/frontend/src/apis/shopping.queries.ts
@@ -1,19 +1,47 @@
 import { queryOptions } from '@tanstack/react-query';
 import { fetcher } from './fetcher';
-import { PurchaseFrequencyRequest, PurchaseFrequencyResponse } from './shopping.types';
+import {
+  CustomerPurchaseRequest,
+  CustomerResponse,
+  PurchaseFrequencyRequest,
+  PurchaseFrequencyResponse,
+} from './shopping.types';
 
 export const shoppingQueryKeys = {
-  purchaseFrequency: ({ from, to }: PurchaseFrequencyRequest) => ['purchase-frequency', from, to],
+  purchase: {
+    all: () => ['purchase-frequency'],
+    purchaseFrequency: ({ from, to }: PurchaseFrequencyRequest) => [
+      ...shoppingQueryKeys.purchase.all(),
+      from,
+      to,
+    ],
+  },
+  customers: {
+    all: () => ['customers'],
+    customers: ({ sortBy, name }: CustomerPurchaseRequest) => [
+      ...shoppingQueryKeys.customers.all(),
+      sortBy,
+      name,
+    ],
+    customersPurchases: (customerId: number) => [...shoppingQueryKeys.customers.all(), customerId],
+  },
 };
 
 export const shoppingQueryOptions = {
   purchaseFrequency: ({ from, to }: PurchaseFrequencyRequest) =>
     queryOptions({
-      queryKey: shoppingQueryKeys.purchaseFrequency({ from, to }),
+      queryKey: shoppingQueryKeys.purchase.purchaseFrequency({ from, to }),
       queryFn: () => {
         return fetcher.get<PurchaseFrequencyResponse>(
           `api/purchase-frequency?from=${from}&to=${to}`,
         );
+      },
+    }),
+  customers: ({ sortBy, name }: CustomerPurchaseRequest) =>
+    queryOptions({
+      queryKey: shoppingQueryKeys.customers.customers({ sortBy, name }),
+      queryFn: () => {
+        return fetcher.get<CustomerResponse>(`api/customers?sortBy=${sortBy ?? ''}&name=${name}`);
       },
     }),
 };
--- apps/frontend/src/apis/shopping.types.ts
+++ apps/frontend/src/apis/shopping.types.ts
@@ -9,3 +9,19 @@ export type Purchase = {
   range: string;
   count: number;
 };
+
+export type CustomerResponse = Customer[];
+
+export type CustomerPurchaseRequest = {
+  sortBy: SortBy | null;
+  name: string;
+};
+
+export type SortBy = 'asc' | 'desc';
+
+export type Customer = {
+  id: number;
+  name: string;
+  count: number;
+  totalAmount: number;
+};
